908ce198b3c66b6af7e6c2ba44bb52eeae53456b
Jake Teton-Landis <just.1.jake@gmail.com>
2023-07-29 19:51:25 -0700

    prettier

--- script/labnol.js
+++ script/labnol.js
@@ -15,42 +15,43 @@ Tutorial: http://www.labnol.org/internet/amazon-price-tracker/28156/
 */
 
 /**
-* @OnlyCurrentDoc
-*/
+ * @OnlyCurrentDoc
+ */
 
 function getConfig() {
   var params = {
-    label:doProperty_("LABEL") || "Unsubscribe"
+    label: doProperty_("LABEL") || "Unsubscribe",
   };
   return params;
 }
 
 function config_() {
-  var html = HtmlService.createHtmlOutputFromFile('config')
-  .setTitle("Gmail Unsubscriber")
-  .setWidth(300).setHeight(200).setSandboxMode(HtmlService.SandboxMode.IFRAME);
+  var html = HtmlService.createHtmlOutputFromFile("config")
+    .setTitle("Gmail Unsubscriber")
+    .setWidth(300)
+    .setHeight(200)
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
   var ss = SpreadsheetApp.getActive();
   ss.show(html);
 }
 
 function help_() {
-  var html = HtmlService.createHtmlOutputFromFile('help')
-  .setTitle("Google Scripts Support")
-  .setWidth(350).setHeight(120);
+  var html = HtmlService.createHtmlOutputFromFile("help")
+    .setTitle("Google Scripts Support")
+    .setWidth(350)
+    .setHeight(120);
   var ss = SpreadsheetApp.getActive();
   ss.show(html);
 }
 
 function createLabel_(name) {
-  
   var label = GmailApp.getUserLabelByName(name);
-  
+
   if (!label) {
     label = GmailApp.createLabel(name);
   }
-  
+
   return label;
-  
 }
 
 function log_(status, subject, view, from, link) {
@@ -59,96 +60,95 @@ function log_(status, subject, view, from, link) {
 }
 
 function init_() {
-  Browser.msgBox("The Unsubscriber was initialized. Please select the Start option from the Gmail menu to activate.");
+  Browser.msgBox(
+    "The Unsubscriber was initialized. Please select the Start option from the Gmail menu to activate.",
+  );
   return;
 }
 
-
 function onOpen() {
-  
-  var menu = [     
-    {name: "Configure", functionName: "config_"},
+  var menu = [
+    { name: "Configure", functionName: "config_" },
     null,
-    {name: "☎ Help & Support",functionName: "help_"},
-    {name: "✖ Stop (Uninstall)",  functionName: "stop_"},
-    null
-  ];  
-  
+    { name: "☎ Help & Support", functionName: "help_" },
+    { name: "✖ Stop (Uninstall)", functionName: "stop_" },
+    null,
+  ];
+
   SpreadsheetApp.getActiveSpreadsheet().addMenu("➪ Gmail Unsubscriber", menu);
-  
 }
 
 function stop_(e) {
-  
   var triggers = ScriptApp.getProjectTriggers();
-  
-  for(var i in triggers) {
+
+  for (var i in triggers) {
     ScriptApp.deleteTrigger(triggers[i]);
   }
-  
+
   if (!e) {
-    Browser.msgBox("The Gmail Unsubscriber has been disabled. You can restart it anytime later."); 
-  }  
+    Browser.msgBox(
+      "The Gmail Unsubscriber has been disabled. You can restart it anytime later.",
+    );
+  }
 }
 
-
 function doProperty_(key, value) {
-  
   var properties = PropertiesService.getUserProperties();
-  
+
   if (value) {
     properties.setProperty(key, value);
   } else {
     return properties.getProperty(key);
   }
-  
 }
 
 function doGmail() {
-  
   try {
-    
     var label = doProperty_("LABEL") || "Unsubscribe";
-    
+
     var threads = GmailApp.search("label:" + label);
-    
+
     var todoLabel = createLabel_(label);
     var successLabel = createLabel_("Unsubscribe Ok");
-    var failLabel = createLabel_("Unsubscribe Fail")
-    
+    var failLabel = createLabel_("Unsubscribe Fail");
+
     var url, urls, message, raw, body, formula, status;
-    
+
     var hyperlink = '=HYPERLINK("#LINK#", "View")';
-    
-    var hrefs = new RegExp(/<a[^>]*href=["'](https?:\/\/[^"']+)["'][^>]*>(.*?)<\/a>/gi);
-    
-    for (var t in threads)  {
-      
+
+    var hrefs = new RegExp(
+      /<a[^>]*href=["'](https?:\/\/[^"']+)["'][^>]*>(.*?)<\/a>/gi,
+    );
+
+    for (var t in threads) {
       url = "";
-      
+
       status = "Could not unsubscribe";
-      
+
       message = threads[t].getMessages()[0];
-      
+
       threads[t].removeLabel(todoLabel);
-      
+
       raw = message.getRawContent();
-      
+
       urls = raw.match(/^list\-unsubscribe:(.|\r\n\s)+<(https?:\/\/[^>]+)>/im);
-      
+
       if (urls) {
         url = urls[2];
         status = "Unsubscribed via header";
       } else {
         body = message.getBody().replace(/\s/g, "");
-        while ( (url === "") && (urls = hrefs.exec(body)) ) {
-          if (urls[1].match(/unsubscribe|optout|opt\-out|remove/i) || urls[2].match(/unsubscribe|optout|opt\-out|remove/i)) {
+        while (url === "" && (urls = hrefs.exec(body))) {
+          if (
+            urls[1].match(/unsubscribe|optout|opt\-out|remove/i) ||
+            urls[2].match(/unsubscribe|optout|opt\-out|remove/i)
+          ) {
             url = urls[1];
             status = "Unsubscribed via link";
           }
         }
       }
-      
+
       if (url === "") {
         urls = raw.match(/^list\-unsubscribe:(.|\r\n\s)+<mailto:([^>]+)>/im);
         if (urls) {
@@ -158,43 +158,39 @@ function doGmail() {
           status = "Unsubscribed via email";
         }
       }
-      
+
       if (status.match(/unsubscribed/i)) {
-        UrlFetchApp.fetch(url, {muteHttpExceptions: true});
-        threads[t].addLabel(successLabel)
+        UrlFetchApp.fetch(url, { muteHttpExceptions: true });
+        threads[t].addLabel(successLabel);
       } else {
-        threads[t].addLabel(failLabel)
+        threads[t].addLabel(failLabel);
       }
-      
+
       formula = hyperlink.replace("#LINK", threads[t].getPermalink());
-      
-      log_( status, message.getSubject(), formula, message.getFrom(), url );
-      
+
+      log_(status, message.getSubject(), formula, message.getFrom(), url);
     }
-  } catch (e) {Logger.log(e.toString())}
-  
+  } catch (e) {
+    Logger.log(e.toString());
+  }
 }
 
-
 function saveConfig(params) {
-  
   try {
-    
     doProperty_("LABEL", params.label);
-    
+
     stop_(true);
-    
-    ScriptApp.newTrigger('doGmail')
-    .timeBased().everyMinutes(15).create();
-    
-    return "The Gmail unsubscriber is now active. You can apply the Gmail label " + params.label + " to any email and you'll be unsubscribed in 15 minutes. Please close this window.";    
-    
+
+    ScriptApp.newTrigger("doGmail").timeBased().everyMinutes(15).create();
+
+    return (
+      "The Gmail unsubscriber is now active. You can apply the Gmail label " +
+      params.label +
+      " to any email and you'll be unsubscribed in 15 minutes. Please close this window."
+    );
   } catch (e) {
-    
     return "ERROR: " + e.toString();
-    
   }
-  
 }
 
 function parseEmail_(email) {
